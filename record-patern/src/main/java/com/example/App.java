e9d013b629565496348917c9e8f0a3078e09542f
apilicita <lexgemix@gmail.com>
2026-04-22 13:35:40 +0200

    SIN record patern

--- record-patern/src/main/java/com/example/App.java
+++ record-patern/src/main/java/com/example/App.java
@@ -1,10 +1,29 @@
 package com.example;
 
-/**
- * Hello world!
- */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    
+	//Metodo que recibe un objeto como parametro y muestra los detalles de dicho objeto
+	//utilizando la sentencia if y Pattern Matching con el metodo instanceof 
+	//y ademas destructure del tipo record que es uno de los record patterns
+	static void imprimeDetalles(Object obj) {
+		if (obj instanceof Book book) {
+			System.out.println("Titulo del libro: "+ book.title());
+			System.out.println("Autor del libro: "+ book.author());
+		}
+	}
+	
+	 
+	public static void main(String[] args) {
+    	
+    //Crear un objeto de tipo record Book
+    //Book libro = new Book("Java es bueno", "Yo mismo");
+    	Book libro = Book.builder()
+    			.title("Java es bueno")
+    			.author("Yo mismo")
+    			.build();
+    	
+    	imprimeDetalles(libro);
+    	
+    	
     }
 }
